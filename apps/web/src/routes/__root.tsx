5cce7ed02b7967e5f1f7c782bbc0d22757a0216d
Mateus Acauhi <mateusacauhi@bluems.com>
2025-09-29 21:53:02 -0300

    chore: remove unused imports from __root.tsx

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { Toaster } from "@/components/ui/sonner";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "@/lib/query-client";
 import { ThemeProvider } from "@/components/theme-provider";
 
@@ -11,9 +10,7 @@ import {
   Scripts,
   createRootRouteWithContext,
   useRouterState,
-  useRouteContext,
 } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import Header from "../components/ui/header";
 import appCss from "../index.css?url";
 import Loader from "@/components/ui/loader";
